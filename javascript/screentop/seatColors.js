ef12b0fa99ce8b66c10e455a6688c1fcd85eba81
Doug Banks <doug.banks@gmail.com>
2026-05-14 14:37:34 -0700

    checking

--- javascript/screentop/seatColors.js
+++ javascript/screentop/seatColors.js
@@ -9,7 +9,29 @@ define(["javascript/parameters", "dojo/domReady!"], function (parameters) {
     "#f58231",
     "#911eb4",
     "#46f0f0",
-    "#ffffff",
+    "#aaaaaa",
+  ];
+
+  var lightenedSeatColors = [
+    "#f28b8b",
+    "#7ed77e",
+    "#ffed8b",
+    "#7a8fe0",
+    "#f9b28b",
+    "#c48de0",
+    "#7ef0f0",
+    "#cccccc",
+  ];
+
+  var darkenedSeatColors = [
+    "#b30f1c",
+    "#1f7a1f",
+    "#ffb300",
+    "#1f3a7a",
+    "#b36f1f",
+    "#5c0e7a",
+    "#1f7e7e",
+    "#666666",
   ];
 
   return {
